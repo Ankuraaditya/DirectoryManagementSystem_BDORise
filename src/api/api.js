069dc9382120d5f3c6f5b1b9444ff8e69c19ed6d
Ankuraaditya <125393079+Ankuraaditya@users.noreply.github.com>
2025-02-09 20:03:20 +0530

    Test Remove

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5245/api"; // Update with your API URL
-
-export const fetchBusinesses = () => axios.get(`${API_URL}/Businesses`);
-export const addBusiness = (data) => axios.post(`${API_URL}/Businesses`, data);
-export const deleteBusiness = (id) => axios.delete(`${API_URL}/Businesses/${id}`);
-export const fetchCategories = () => axios.get(`${API_URL}/Category`);
